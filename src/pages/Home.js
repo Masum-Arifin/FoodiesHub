add1c5394632f526b51e785630141cadb1d95ea9
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-11 19:30:57 +0600

    new code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-import HeroSection from "../components/HeroSection";
-import ImproveSkills from "../components/ImproveSkills";
-import QouteSection from "../components/QuoteSection";
-import ChiefsSection from "../components/ChiefsSection";
+// import HeroSection from "../components/HeroSection";
+// import ImproveSkills from "../components/ImproveSkills";
+// import QouteSection from "../components/QuoteSection";
+// import ChiefsSection from "../components/ChiefsSection";
 
 
-export default function Home(){
-    return (
-        <div>
-            <HeroSection />
-            <ImproveSkills />
-            <QouteSection />
-            <ChiefsSection />
-        </div>
-    )
-}
+// export default function Home(){
+//     return (
+//         <div>
+//             <HeroSection />
+//             <ImproveSkills />
+//             <QouteSection />
+//             <ChiefsSection />
+//         </div>
+//     )
+// }
