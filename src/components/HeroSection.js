445c2f79dfb1ddc07bd79339c330db3091357b4c
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-12 19:37:23 +0600

    new code

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,11 +19,11 @@ export default function HeroSection(){
                 <p className="info">FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free. So start exploring now.</p>
                 <button className="btn">explore now</button>
             </div>
-            {/* <div className="col gallery">
+            <div className="col gallery">
                 { images.map((src, index) => (
                     <CustomImage key={index} imgSrc={src} pt={"90%"} />
                 )) }
-            </div> */}
+            </div>
         </div>
     )
 }
