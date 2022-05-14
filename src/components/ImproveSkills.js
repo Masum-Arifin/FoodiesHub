9290c1dfdf926f4c2b07800afc0c23a36fcdd5a4
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-14 19:50:58 +0600

    new code

--- src/components/ImproveSkills.js
+++ src/components/ImproveSkills.js
@@ -16,9 +16,9 @@ export default function ImproveSkills(){
             <div className="col typography">
                 <h1 className="title">Improve Your Culinary Skills</h1>
                 { list.map((item, index) => (
-                    <p className="skill-item" key={index}>{item}</p>
+                    // <p className="skill-item" key={index}>{item}</p>
                 )) }
-                {/* <button className="btn">Signup Now</button> */}
+                <button className="btn">Signup Now</button>
             </div>
         </div>
     )
