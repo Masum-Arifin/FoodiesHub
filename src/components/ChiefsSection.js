3d540aa75ce2b5fdd03f5f82d7b7573934e38bbf
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-16 20:47:38 +0600

    new code

--- src/components/ChiefsSection.js
+++ src/components/ChiefsSection.js
@@ -14,18 +14,18 @@ export default function ChiefsSection(){
             recipesCount: "05",
             cuisine: "Japanese",
         },
-        {
-            name: "Erich Maria",
-            img: "/img/top-chiefs/img_3.jpg",
-            recipesCount: "13",
-            cuisine: "Italian",
-        },
         // {
-        //     name: "Chris Brown",
-        //     img: "/img/top-chiefs/img_4.jpg",
-        //     recipesCount: "08",
-        //     cuisine: "American"
+        //     name: "Erich Maria",
+        //     img: "/img/top-chiefs/img_3.jpg",
+        //     recipesCount: "13",
+        //     cuisine: "Italian",
         // },
+        {
+            name: "Chris Brown",
+            img: "/img/top-chiefs/img_4.jpg",
+            recipesCount: "08",
+            cuisine: "American"
+        },
         {
             name: "Blake Lively",
             img: "/img/top-chiefs/img_5.jpg",
