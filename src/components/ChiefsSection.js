59833d52ff658548a463395d03e2259b7a6ea637
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-16 20:01:43 +0600

    new code

--- src/components/ChiefsSection.js
+++ src/components/ChiefsSection.js
@@ -41,8 +41,8 @@ export default function ChiefsSection(){
     ]
     return (
         <div className="section chiefs">
-            <h1 className="title">Our Top Chiefs</h1>
-            {/* <div className="top-chiefs-container">
+            {/* <h1 className="title">Our Top Chiefs</h1> */}
+            <div className="top-chiefs-container">
                 {/* <ChiefCard />
                 <ChiefCard />
                 <ChiefCard />
@@ -50,7 +50,7 @@ export default function ChiefsSection(){
                 <ChiefCard />
                 <ChiefCard /> */}
                 { chiefs.map(chief => <ChiefCard key={chief.name} chief={chief} />) }
-            </div> */}
+            </div>
         </div>
     )
 }
