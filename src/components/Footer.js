7fcab78e994f19f8b7707333b040828a8c4ff12a
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-18 20:57:02 +0600

    new code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer(){
         <div className="footer container">
             <div className="footer-section">
                 <p className="title">FoodiesHub.com</p>
-                <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
+                {/* <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p> */}
                 <p>&copy; 2021 | All Rights Reserved</p>
             </div>
             <div className="footer-section">
