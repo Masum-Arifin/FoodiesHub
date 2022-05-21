fb7b0e6d271e9702aed9a1e4bd70467b91e29eda
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-21 21:41:31 +0600

    new code

--- src/components/ChiefCard.js
+++ src/components/ChiefCard.js
@@ -10,7 +10,7 @@ export default function ChiefCard({chief}) {
                 <h3 className="chief-card-name">{chief.name}</h3>
                 <p className="chief-recipe-count">Recipes: <b>{chief.recipesCount}</b></p>
                 <p className="chief-cuisine">Cuisine: <b>{chief.cuisine}</b></p>
-                {/* <p className="cheif-icons"> */}
+                <p className="cheif-icons">
                     <FontAwesomeIcon icon={faFacebook} />
                     <FontAwesomeIcon icon={faTwitter} />
                     <FontAwesomeIcon icon={faInstagram} />
